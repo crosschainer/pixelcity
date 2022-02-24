58a1d33fb4817ab1ee96db47eeafaf3facadf02d
root <eodkwokm@gmail.com>
2022-02-24 10:03:38 +0000

    New house

---
+++ test/con_pixelcity_staking.py
@@ -0,0 +1,26 @@
+I = importlib
+
+pixelcity_contract = Variable()
+mob_contract = Variable()
+staking_rewards = Hash(default_value=0)
+
+@construct
+def seed():
+    pixelcity_contract.set("con_pixelcity_master_1")
+    mob_contract.set("con_mintorburn")
+
+def depositRewards(mob_amount: float):
+    assert mob_amount > 0, "Cant deposit negative amounts"
+    I.import_module(mob_contract.get()).transfer_from(amount=mob_amount, to=ctx.this, main_account=ctx.caller)
+    plots = ForeignHash(foreign_contract=pixelcity_contract.get(), foreign_name='plots')
+    for plot_thing in ForeignVariable(foreign_contract=pixelcity_contract.get(), foreign_name='valid_plot_nfts'):
+        if(plots[plot_thing] != 0):
+            if(plots[plot_thing]["built"] != ""):
+                staking_rewards[plots[plot_thing]["current_build_owner"]] += mob_amount / (len(ForeignVariable(foreign_contract=pixelcity_contract.get(), foreign_name='in_use_building_nfts')))
+
+@export
+def claim_rewards():
+    amount_to_claim = staking_rewards[ctx.caller]
+    if(amount_to_claim > 0):
+        staking_rewards[ctx.caller] = 0
+        I.import_module(mob_contract.get()).transfer(amount=amount_to_claim, to=ctx.caller)
